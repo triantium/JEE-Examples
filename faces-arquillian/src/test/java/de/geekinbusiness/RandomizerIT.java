bf83773372faf5b670492242b6134b769f027147
Manuel Müller <manuel.mueller@geekinbusiness.de>
2017-09-24 11:08:38 +0200

    -working fakeServingServlet
    Signed-off-by:Manuel Müller <manuel.mueller@geekinbusiness.de>

--- faces-arquillian/src/test/java/de/geekinbusiness/RandomizerIT.java
+++ faces-arquillian/src/test/java/de/geekinbusiness/RandomizerIT.java
@@ -1,9 +1,13 @@
 package de.geekinbusiness;
 
+import java.io.File;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,14 +20,29 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class RandomizerIT {
 
+    private static final String WEBAPP_SRC = "src" + File.separator + "main" + File.separator + "webapp" + File.separator + "";
+
     @Inject
     Randomizer rand;
 
     @Deployment
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "faces-arquillian.war")
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, "faces-arquillian.war")
                 .addClass(Randomizer.class)
-                .addAsResource("test-persistence.xml", "META-INF/persistence.xml");
+                .addClass(FakeServlet.class);
+//                .addAsResource("test-persistence.xml", "META-INF/persistence.xml");
+
+//        File[] files = Maven.resolver().loadPomFromFile("pom.xml")
+//                .importRuntimeDependencies().resolve().withTransitivity().asFile();
+//
+//        archive.addAsLibraries(files);
+        archive.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+                .importDirectory(WEBAPP_SRC).as(GenericArchive.class),
+                "/", Filters.includeAll());
+
+        System.out.println("WebArchiv erstellt");
+        return archive;
+
     }
 
     @Before
